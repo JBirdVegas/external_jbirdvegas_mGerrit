b1e72b6a7241cebf8c39dfb5b1e008b6f1035797
Stanford, Jon <Jonathan.Stanford@asurion.com>
2016-03-14 01:47:28 -0500

    rename static variables with 's' prefix instead of 'm'

--- app/src/main/java/com/jbirdvegas/mgerrit/helpers/AnalyticsHelper.java
+++ app/src/main/java/com/jbirdvegas/mgerrit/helpers/AnalyticsHelper.java
@@ -4,38 +4,38 @@ import android.app.Activity;
 import android.content.Context;
 
 public class AnalyticsHelper {
-    private static AnalyticsHelper mInstance;
-    private static AnalyticsSender mHelper;
+    private static AnalyticsHelper sInstance;
+    private static AnalyticsSender sAnalyticsSender;
 
     private AnalyticsHelper() {
         // keep out
     }
 
     public static AnalyticsHelper getInstance() {
-        if (mHelper == null) {
+        if (sAnalyticsSender == null) {
             // sender implementation depends on productFlavor.  `googlePlay` implements the analytics where
             // `noAnalytics` stubs all methods to avoid collecting analytics for those who prefer to not be tracked
-            mHelper = new AnalyticsSenderImpl();
+            sAnalyticsSender = new AnalyticsSenderImpl();
         }
-        if (mInstance == null) {
-            mInstance = new AnalyticsHelper();
+        if (sInstance == null) {
+            sInstance = new AnalyticsHelper();
         }
-        return mInstance;
+        return sInstance;
     }
 
     public void sendAnalyticsEvent(Context context, String category, String action, String label, Long value) {
-        mHelper.sendAnalyticsEvent(context, category, action, label, value);
+        sAnalyticsSender.sendAnalyticsEvent(context, category, action, label, value);
     }
 
     public void startActivity(Activity activity) {
-        mHelper.startActivity(activity);
+        sAnalyticsSender.startActivity(activity);
     }
 
     public void stopActivity(Activity activity) {
-        mHelper.stopActivity(activity);
+        sAnalyticsSender.stopActivity(activity);
     }
 
     public void initAnalytics(Context context) {
-        mHelper.initAnalytics(context);
+        sAnalyticsSender.initAnalytics(context);
     }
 }
