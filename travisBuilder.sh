c266207b6a618376ad1bfea6bbbbfa29dc5f5056
Evan Conway <p4r4n01d42@gmail.com>
2016-06-05 12:30:58 +1000

    Use the gradle wrapper to build with Travis not the default gradle version
    Change-Id: I2b0355595ef95ff65cb3f97ab994883c9cf33261

--- travisBuilder.sh
+++ travisBuilder.sh
@@ -3,12 +3,15 @@
 #Simple script to detect the current branch.  This is used by TavisCI server to determine if
 #release actions should be performed.
 
+# Use the gradle wrapper (gradlew) to build with Travis to ensure we have the correct gradle version
+# See: https://docs.gradle.org/current/userguide/gradle_wrapper.html
+
 if [[ $(git branch) == "master" ]]; then
     echo "Master build detected!  Preparing release build.";
-    gradle clean fullRelease
+    ./gradlew clean fullRelease
 fi
 
 if [[ $(git branch) != "master" ]]; then
     echo "Branch build detected.  Compile, package and perform tests but in the end we don't care about the result";
-    gradle clean assembleGooglePlay
+    ./gradlew clean assembleGooglePlay
 fi
